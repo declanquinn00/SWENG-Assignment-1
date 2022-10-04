e8787d0d233641a7faad43a043582a51ae763902
declanquinn00 <quinndeclan00@gmail.com>
2022-10-04 15:47:08 +0100

    added stacks and ability to push values to stacks

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,37 @@
+import java.util.*;
 public class Calculator {
+    
+    Stack<Character> opStack;
+    Stack<Character> valStack;
+
+    // Constructor create two stacks
+    private Calculator(){
+        Stack<Character> opStack = new Stack<>();
+        Stack<Character> valStack = new Stack<>();
+        this.opStack = opStack;
+        this.valStack = valStack;
+    }
     public String equate(String input){
+        // read string
+        for(int i = 0; i <= input.length()-1; i++){
+            char val = input.charAt(i);
+            pushToStacks(val);
+        }
+        // 
         return input; // RETURN A STRING REPRESENTATION OF ANSWER
+
+    }
+
+    public void pushToStacks(Character op){
+        try{
+        // Check if operatoe
+        if(op == '+' || op == '-' || op == 'X' || op == '/'){
+            opStack.push(op);
+        }
+        // Else push to valStack
+        else{
+            valStack.push(op);
+        }
+        } catch (java.lang.Exception e){System.out.println("Error: Unable to push to satck");}
     }
 }
