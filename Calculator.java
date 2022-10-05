5a87db4d3cf46a94bfb83326777cd9a2fe0703ca
declanquinn00 <quinndeclan00@gmail.com>
2022-10-06 00:53:31 +0100

    Worked on creating infix calculator

--- Calculator.java
+++ Calculator.java
@@ -1,13 +1,13 @@
 import java.util.*;
 public class Calculator {
     
-    Stack<Character> opStack;
+    Stack<String> opStack;
     Stack<Integer> valStack;
     ArrayList<String> array;
 
     // Constructor create two stacks
     public Calculator(){
-        Stack<Character> opStack = new Stack<>();
+        Stack<String> opStack = new Stack<>();
         Stack<Integer> valStack = new Stack<>();
         ArrayList<String> array = new ArrayList<String>();
         this.array = array;
@@ -69,17 +69,104 @@ public class Calculator {
         else return false;
     }
 
-    public void pushToStacks(Character op){
+    public void pushToStacks(String op){
         try{
         // Check if operator
-        if(op == '+' || op == '-' || op == '*' || op == '/'){
+        if(op == "+" || op == "-" || op == "*" || op == "/"){
             opStack.push(op);
         }
         // Else push to valStack
         else{
-            //valStack.push(op);
+            valStack.push(Integer.parseInt(op));
         }
         } catch (java.lang.Exception e){System.out.println("Error: Unable to push to satck");}
+    }
+
+    // Evaluates infix expression by converting to postfix
+    public void evaluateInfix(String infix){
+        toArray(infix);
+        String output = "";
+        String value;
+        String tmp;
+        Boolean first = true;
+        for(int i=0; i<array.size()-1; i++){
+            value = array.get(i);
+            // If "(" push onto operator stack
+            if(value.equalsIgnoreCase("(")){
+                pushToStacks(value);
+            }
+            // If ")" pop off items until "("
+            else if(value.equalsIgnoreCase(")")){
+                while(opStack.peek()!="("){ // !!
+                    tmp = opStack.pop();
+                    output += tmp;
+                }
+                opStack.pop();
+            }
+            // If operator run check
+            else if(isOperator(value)){
+                // If first value add to stack
+                if(opStack.peek()!=null){  // first?
+                    opStack.push(value);
+                    first = false;
+                }
+                // If operator has greater prescedence than top of stack pop off top of stack and push operator
+                else{
+                    String top = opStack.peek();
+                    while(opPrecedenceGreaterOrEqual(top, value)){
+                        output += opStack.pop();
+                    }
+                    opStack.push(value);
+                }
+            }
+            // If operand add to string
+            else{
+                output += value;
+            }
+        }
+        System.out.println("Testing output:" + output);
+
+        
+    }
+
+    // Assigns value to operator and returns if it is greater or equal to another
+    Boolean opPrecedenceGreaterOrEqual(String op1, String op2){
+        int op1Val = 0;
+        int op2Val = 0;
+
+        switch(op1){
+            case "*":
+                op1Val = 2;
+                break;
+            case "/":
+                op1Val = 2;
+                break;
+            case "+":
+                op1Val =1;
+                break;
+            case "-":
+                op1Val =1;
+                break;
+        }
+
+        switch(op2){
+            case "*":
+                op2Val = 2;
+                break;
+            case "/":
+                op2Val = 2;
+                break;
+            case "+":
+                op2Val =1;
+                break;
+            case "-":
+                op2Val =1;
+                break;
+        }
+        if(op1Val >= op2Val && op1Val !=0 && op2Val !=0){
+            return true;
+        }
+        else return false;
     }
 
     // Evaluates postfix expression
